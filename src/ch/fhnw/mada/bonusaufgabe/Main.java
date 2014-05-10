fce89f24c6300a1365b6c24df64038fe4e67de67
Raphael Bolliger <raphi@raphibolliger.ch>
2014-05-10 23:36:08 +0200

    initial commit

---
+++ src/ch/fhnw/mada/bonusaufgabe/Main.java
@@ -0,0 +1,16 @@
+package ch.fhnw.mada.bonusaufgabe;
+
+/**
+ * Erstellt von raphi am 10.05.14.
+ * Version : 1.0.0
+ * Modul: mada
+ */
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+
+    }
+
+}
