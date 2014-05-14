433ae216e2fddf55a31357f7368bd383f3ad52c4
Raphael Bolliger <raphi@raphibolliger.ch>
2014-05-14 12:08:28 +0200

    constructor for decode

--- src/ch/fhnw/mada/bonusaufgabe/logic/Decode.java
+++ src/ch/fhnw/mada/bonusaufgabe/logic/Decode.java
@@ -1,13 +1,10 @@
 package ch.fhnw.mada.bonusaufgabe.logic;
 
-/**
- * Erstellt von raphi am 11.05.14.
- * Version : 1.0.0
- * Modul: algd1
- */
 public class Decode {
 
+    public Decode()
+    {
 
-
+    }
 
 }
