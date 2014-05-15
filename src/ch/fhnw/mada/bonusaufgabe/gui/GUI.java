2da61fa4dd31122f7cf334166d2d2cffc5b239a4
Raphael Bolliger <raphi@raphibolliger.ch>
2014-05-15 12:50:49 +0200

    new gui

--- src/ch/fhnw/mada/bonusaufgabe/gui/GUI.java
+++ src/ch/fhnw/mada/bonusaufgabe/gui/GUI.java
@@ -1,7 +1,5 @@
 package ch.fhnw.mada.bonusaufgabe.gui;
 
-import com.sun.imageio.plugins.gif.GIFImageReader;
-
 import javax.swing.*;
 import java.awt.*;
 
@@ -10,9 +8,16 @@ public class GUI extends JFrame {
     private JPanel encodePanel = new encodePanel();
     private JPanel decodePanel = new JPanel();
 
+    private JTextField inputFile = new JTextField("Bitte wählen Sie eine ASCII formatierte Textdatei aus");
+    private JButton chooseInputFile = new JButton("Wählen...");
+    private JTextField outputPath = new JTextField("Bitte wählen Sie einen Ausgabefad aus");
+    private JButton chooseOutputPath = new JButton("Wählen...");
+    private JCheckBox openAfterEncode = new JCheckBox("Dateien nach dem Kodieren öffnen");
+    private JButton buttonEncode = new JButton("Kodieren");
+
     public GUI()
     {
-        setTitle("Encode/decode textfiles");
+        setTitle("Mathematik für die Datenkommunikation");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setPreferredSize(new Dimension(400,400));
 
@@ -32,19 +37,20 @@ public class GUI extends JFrame {
         containerPanel.setLayout(new GridLayout(3,1));
 
         JPanel temp1 = new JPanel();
-        temp1.setLayout(new GridLayout(1,2));
-        temp1.add(new JTextField("Bitte wählen Sie eine ASCII formatierte Textdatei aus"));
+        temp1.setLayout(new FlowLayout());
+        inputFile.setPreferredSize(new Dimension(400,20));
+        temp1.add(inputFile);
         temp1.add(new JButton("Wählen..."));
         containerPanel.add(temp1);
 
         JPanel temp2 = new JPanel();
-        temp2.setLayout(new GridLayout(1,2));
+        temp2.setLayout(new FlowLayout());
         temp2.add(new JTextField("Bitte wählen Sie einen Ausgabepfad"));
         temp2.add(new JButton("Wählen..."));
         containerPanel.add(temp2);
 
         JPanel temp3 = new JPanel();
-        temp3.setLayout(new GridLayout(1,2));
+        temp3.setLayout(new FlowLayout());
         temp3.add(new JCheckBox("Datei nach dem Kodieren öffnen..."));
         temp3.add(new JButton("Kodieren"));
         containerPanel.add(temp3);
