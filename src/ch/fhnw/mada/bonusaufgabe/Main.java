56bde1d7d40ec4e3c2e681eb97cd03ea4ff7d5ca
Raphael Bolliger <raphi@raphibolliger.ch>
2014-05-15 09:26:29 +0200

    new gui

--- src/ch/fhnw/mada/bonusaufgabe/Main.java
+++ src/ch/fhnw/mada/bonusaufgabe/Main.java
@@ -1,12 +1,14 @@
 package ch.fhnw.mada.bonusaufgabe;
 
 import ch.fhnw.mada.bonusaufgabe.gui.GUI;
+import ch.fhnw.mada.bonusaufgabe.gui.MainForm;
 
 public class Main {
 
     public static void main(String[] args)
     {
         new GUI();
+        //new MainForm();
     }
 
 }
--- src/ch/fhnw/mada/bonusaufgabe/gui/GUI.java
+++ src/ch/fhnw/mada/bonusaufgabe/gui/GUI.java
@@ -1,5 +1,7 @@
 package ch.fhnw.mada.bonusaufgabe.gui;
 
+import com.sun.imageio.plugins.gif.GIFImageReader;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -14,10 +16,50 @@ public class GUI extends JFrame {
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setPreferredSize(new Dimension(400,400));
 
-        setLayout(new GridLayout(1,2));
+        setLayout(new BorderLayout());
 
-        add(encodePanel);
-        add(decodePanel);
+        // add title on top
+        Font font = new Font("Verdana", Font.BOLD, 18);
+        JLabel title = new JLabel("Kodieren und Dekodieren von ASCII Textdateien", JLabel.CENTER);
+        title.setFont(font);
+        add(title, BorderLayout.NORTH);
+
+        // create kodieren for tabbedPane as a JPanel
+        JPanel panelKodieren = new JPanel();
+        panelKodieren.setLayout(new BorderLayout());
+
+        JPanel containerPanel = new JPanel();
+        containerPanel.setLayout(new GridLayout(3,1));
+
+        JPanel temp1 = new JPanel();
+        temp1.setLayout(new GridLayout(1,2));
+        temp1.add(new JTextField("Bitte wählen Sie eine ASCII formatierte Textdatei aus"));
+        temp1.add(new JButton("Wählen..."));
+        containerPanel.add(temp1);
+
+        JPanel temp2 = new JPanel();
+        temp2.setLayout(new GridLayout(1,2));
+        temp2.add(new JTextField("Bitte wählen Sie einen Ausgabepfad"));
+        temp2.add(new JButton("Wählen..."));
+        containerPanel.add(temp2);
+
+        JPanel temp3 = new JPanel();
+        temp3.setLayout(new GridLayout(1,2));
+        temp3.add(new JCheckBox("Datei nach dem Kodieren öffnen..."));
+        temp3.add(new JButton("Kodieren"));
+        containerPanel.add(temp3);
+
+        panelKodieren.add(containerPanel, BorderLayout.NORTH);
+
+        // create dekodieren for tabbedPane as a JPanel
+
+
+        // add tabbedPane on top
+        JTabbedPane tabbedPane = new JTabbedPane();
+        tabbedPane.addTab("Kodieren", panelKodieren);
+        tabbedPane.addTab("Dekodieren", new JPanel());
+        add(tabbedPane, BorderLayout.CENTER);
+
 
         pack();
         setVisible(true);
